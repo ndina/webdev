4a336089d639d45c7d1d1efbe998c9f7c6058cfb
ndina <dbom12360@gmail.com>
2019-04-09 00:39:50 +0600

    todo-front

---
+++ week11/todo-front/src/app/main/models/todo.ts
@@ -0,0 +1,12 @@
+export interface TaskList {
+    id:number;
+    name:string;
+}
+export interface Task{
+    id:number;
+    name:string;
+    created_at:any;
+    due_on:any;
+    status:string;
+
+}
---
+++ week11/todo-front/src/app/main/services/provider.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { MyServiceService } from './myservice.service';
+import { HttpClient } from '@angular/common/http'
+import { TaskList, Task } from '../models/todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProviderService extends MyServiceService {
+
+  constructor(http: HttpClient) {
+    super(http);
+   }
+
+   getTaskLists(): Promise<TaskList[]> {
+    return this.get('http://127.0.0.1:8000/api/task_lists',  {});
+  }
+  getTasks(id: number) {
+    return this.get(`http://localhost:8000/api/task_lists/${id}/tasks/`, {});
+  }
+
+}
